beda21c256f110f6ca97e494f34d6d6d8ede0c36
RubenC2 <rubenct1993@gmail.com>
2025-01-26 23:31:10 +0100

    fixed Axios

--- src/components/Main/AdminDashboard/AdminDashboard.jsx
+++ src/components/Main/AdminDashboard/AdminDashboard.jsx
@@ -35,7 +35,7 @@ const ArticuloAdd = () => {
 
     try {
 
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/articulos`, articuloData);
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/articulos`, articuloData);
       console.log("Artículo guardado:", response.data);
 
       setMessage("Artículo creado exitosamente!");
